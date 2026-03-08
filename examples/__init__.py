e5c8c08728bbe4ee2f4b7b76b7084999cd3b99f0
Kay Gijzen <58943291+Kaygijzen@users.noreply.github.com>
2026-03-08 18:42:39 +0100

    occlusion generation demo notebook

---
+++ examples/__init__.py
@@ -0,0 +1,9 @@
+"""
+Examples for OASIC occlusion utilities.
+
+This directory contains Jupyter notebooks demonstrating how to use
+the occlusion generation and detection libraries:
+
+- occlusion_generation_demo.ipynb: Creating synthetic occlusions (gray occlusions, overlay occlusions)
+- occlusion_detection_demo.ipynb: Detecting and localizing occlusions using anomaly detection
+"""
